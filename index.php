4eaf0ab810db3ce97d799d66fe91fcb631454309
Wilssoooonnn <adamnuralifi11@gmail.com>
2024-11-29 01:52:10 +0700

    Update login form

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style_login.css">
+    <link rel="stylesheet" href="assets/css/style_font.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <?php
+    // include 'includes/header.php';
+    include 'pages/login.php';
+    // include 'includes/footer.php';
+    ?>
+</body>
+<script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</html>
---
+++ pages/login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <div class="card w-50 mx-auto">
+        <div class="card-body">
+            <img src="assets/images/logo.svg" alt="logo" class="mx-auto d-block mb-5">
+            <form action="#" method="post">
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    <input type="text" class="form-control" id="username" name="username" required>
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" class="form-control" id="password" name="password" required>
+                </div>
+                <div class="form-group form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="">Remember me</label>
+                </div>
+                <div class="">
+                    <button type="submit" class="btn btn-primary mt-3">Login</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>

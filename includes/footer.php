ce4210d92e75965ac84f8c47814ff247e32cd0a4
Wilssoooonnn <adamnuralifi11@gmail.com>
2024-11-29 10:19:46 +0700

    deleted: delete unnecessary files

--- includes/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="bg-dark text-white text-center py-3">
-   Footer
-</footer>
-<!-- Bootstrap JS -->
-<script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="assets/js/script.js"></script>
-</body>
-
-</html>
